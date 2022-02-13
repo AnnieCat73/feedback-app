7e347c99342ccfbd7701ebd9d2e44ee9c433031b
Annie <devannie777@gmail.com>
2022-02-13 05:48:38 +0000

    Conditionals added to Feedback practice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
+import { buildQueries } from '@testing-library/react';
 import React from 'react'
 
+
 function Header(props) {
+
+  const paragraphStyles = { //styled in a variable passed in below
+    backgroundColor: 'green', 
+    color: 'red',
+  }
+
+  const propsStyle = {//could add a prop style like this and add it to below and then link it in app like <Header propsStyle="2rem"
+   fontSize: fontSize,
+  }
   return (
     
-    <header>
+    <header style={{backgroundColor: 'blue', color: 'red'}}>{/*Inline styleing*/}
       <div className="container">
-        <h2>{props.text}</h2>
+        <h2>{props.text,propsStyle}</h2>
+        <p>{paragraphStyles}Hello</p>
       </div>
     </header>
 
