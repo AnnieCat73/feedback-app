b8af46dda9e0d65dd5d6bf1fa28b89a7dfaa32fb
Annie <devannie777@gmail.com>
2022-02-17 05:09:33 +0000

    State first steps added to FeedbackItem

--- src/Myfolder/state.js
+++ src/Myfolder/state.js
@@ -1,2 +1,30 @@
 //Component level state - only accessed in the component it is in not in. Can'taccess it from other components etc - f.ex a is-navigiation-open is set to true or false
 //Global or App level state - need to use a state from a components in other components - so share the state among multiple components
+
+//with { useState } hook
+
+import React from 'react'
+import { useState } from 'react';//1) UseState hook
+
+function FeedbackItem() {
+  
+  const [rating, setRating] = useState(7);//2) SETTING STATE USING DESTRUCTURING/a) new variable declared, rating, b)and it's initial state we set to 7. C) setRating is a function we set up to update the state rating
+  const [text, setText] = useState('This is an example of a feedback item');
+
+  const handleClick = () => { //5)what function does when click on button
+    setRating((prev) => {       
+      console.log(prev);      //Here we use setRating defined above
+      return prev + 1;        //can set prev as in previous value if need access to it
+    });                       //here returns previous value i.e 7 and adds 1
+  }
+
+  return (
+    <div className='card'>
+      <div className="num-display">{rating}</div>{/*3)Where rating state goes*/} 
+      <div className='text-display'>{text}</div>
+      <button onClick={handleClick}>Click</button>{/*4)Where we fire off a function to change state*/} 
+    </div>
+  )
+}
+
+export default FeedbackItem;
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { useState } from 'react';
 
 function FeedbackItem() {
-  const [rating, setRating] = useState(7);//SETTING STATE USING DESTRUCTURING
+  
+  const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of a feedback item');
+
+  
+
   return (
     <div className='card'>
       <div className="num-display">{rating}</div>
       <div className='text-display'>{text}</div>
-
+      
     </div>
   )
 }
