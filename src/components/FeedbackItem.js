1c59f922906a046282a7ffab3df74261739fbbe4
Annie <devannie777@gmail.com>
2022-02-18 06:35:25 +0000

    Feedback list issues sorted

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -13,3 +13,17 @@ function FeedbackItem({ item }) {
 }
 
 export default FeedbackItem;
+
+/*SAME AS 
+function FeedbackItem(props) {
+  
+  return (
+    <div className='card'>
+      <div className="num-display">{props.rating}</div>
+      <div className='text-display'>{props.text}</div>
+      
+    </div>
+  )
+}
+
+export default FeedbackItem;*/
