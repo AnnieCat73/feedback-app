3e8d6333ce1528f276f07a8429853ce93625dfb6
Annie <devannie777@gmail.com>
2022-03-04 06:26:35 +0000

    JSX practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { useState } from 'react';
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackData from './data/FeedbackData';
+//import { useState } from 'react';
+//import Header from "./components/Header";
+//import FeedbackList from "./components/FeedbackList";
+//import FeedbackData from './data/FeedbackData';
 import './index.css';
 
-function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
 
+const comments = [
+  {id: 1, text: "Comment one"},
+  {id: 2, text: "Comment two"},
+  {id: 3, text: "Comment three"}
+]
+
+const moreComments = comments.map((comment, index) => (
+  <li key={index}>{comment.text}</li>
+))
+function App() {
+  const title = "Blogs";
+  
   return (
     <div>
-      <Header />
-      <div className='container'>
-      
-        <FeedbackList feedback={feedback}/>
-
-      
-      </div>
+      <h1>{title}</h1>
+      {/*}<ul>
+        {comments.map((comment,index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+        </ul>*/}
+      <ul>{moreComments}</ul>     
     </div>
     
     
