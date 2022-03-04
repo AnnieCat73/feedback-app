e3eeae16a63c52c2d04a5dfbb37aa5b6ccc9f057
Annie <devannie777@gmail.com>
2022-03-04 06:49:58 +0000

    JSX practice part 2

--- src/Myfolder/Myexercises.js/exercises.js
+++ src/Myfolder/Myexercises.js/exercises.js
@@ -1,10 +1,4 @@
-//import { useState } from 'react';
-//import Header from "./components/Header";
-//import FeedbackList from "./components/FeedbackList";
-//import FeedbackData from './data/FeedbackData';
 import './index.css';
-
-
 
 const animals = [
   {id: 1, type: "cat", color: "white"},
@@ -12,30 +6,29 @@ const animals = [
   {id: 3, type: "hamster", color: "beige"}
 ];
 
+const animalsAll = animals.map((animal, index) => {
+  return <li key={index}>{animal.type} and {animal.color}</li>
 
+}) //1) CAN BE DEFINED HERE WITH  return and { OR
 
 
 function App() {
-  const title = "Blogs";
-  {/*}const animalsAll = animals.map((animal, index) => (
+  const animalsAll = animals.map((animal, index) => (
      <li key={index}>{animal.type}{animal.color}</li>
+  )) // 2) OR AS JSX WITH ( and without return here
 
-  ))*/}
   const animalsAll = animals.map((animal, index) => {
     return <li key={index}>{animal.type} and {animal.color}</li>
+  })//1 CAN ALSO BE DEFINED HERE WITH return AND {
   
-  })
   return (
     <div>
-      <h1>{title}</h1>
       <ul>
         {animalsAll}
       </ul>
-     
     </div>
     
     
   );
 }
-
 export default App;
