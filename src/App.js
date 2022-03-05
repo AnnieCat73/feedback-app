ea339fe4872d9b3fae0449cbb77f77abdd7a46d9
Annie <devannie777@gmail.com>
2022-03-05 06:02:05 +0000

    Styling exercise set up

--- src/App.js
+++ src/App.js
@@ -2,36 +2,22 @@
 //import Header from "./components/Header";
 //import FeedbackList from "./components/FeedbackList";
 //import FeedbackData from './data/FeedbackData';
+import React from 'react';
 import './index.css';
-
-
-const numbers = [1, 2, 3, 4, 5]
+import Styling from './Myfolder/Exercises/Styling';
 
 
 
 function App() {
-  const happy = true;
-  const unhappy = true;
-
-  if(happy) return <h1>Happy....</h1>
   
+
  
   return (
-    <div>
-      {unhappy ? (
-          <div>
-          <h3>{numbers.length}</h3>
-          <ul>
-            {numbers.map((number, index) => (
-              <li key={index}>{number}</li>
-            ))};
-          </ul>
-        </div>
-      ) : (
-        'yes' 
-      )}
-    </div>
-    
+   <div>
+     <Styling bgColor="pink" textColor="white"/>
+   </div>
+      
+   
     
   );
 }
---
+++ src/Myfolder/Exercises/Styling.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Styling() {
+  return (
+    <header>
+      <div>
+        <h2></h2>
+        <p></p>
+      </div>
+    </header>
+    
+  )
+}
+
+export default Styling
