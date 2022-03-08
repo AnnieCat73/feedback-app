f5ed965c3f6d519996f812ab4d60552f710fb8a4
Annie <devannie777@gmail.com>
2022-03-08 05:20:55 +0000

    State part 1

--- src/Myfolder/Exercises/Feedback.js
+++ src/Myfolder/Exercises/Feedback.js
@@ -1,5 +1,6 @@
 //import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 
 function Feedback() {
 
@@ -11,12 +12,22 @@ function Feedback() {
     });
   }
 
-  const [text, setText] = useState("Please explain the rating you gave!")
+  const [text, setText] = useState("");
+  
+  useEffect(() => {
+    console.log(text)
+  })
+
 
   return (
     <div style={{display: 'flex', justifyContent: 'space-around', paddingTop: '3rem'}}>
       <h2>Rating: {rating}</h2>
-      <p>An example: {text}</p>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <p>Please explain the rating you gave:</p>
+        <input style={{maxWidth: '7rem', marginLeft: '1em', height: '1.2rem'}} value={text} onChange={e => setText(e.target.value)}/>
+      </div>
+      
+
       <button onClick={handleRating}>Give Feedback</button>
 
     </div>
